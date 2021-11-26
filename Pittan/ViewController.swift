8a44acd77b70bb52ddb3f0c5d9a8f1213272bc31
Taguchi-wq <20cm0123@jec.ac.jp>
2021-11-26 13:36:24 +0900

    first commit

---
+++ Pittan/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Pittan
+//
+//  Created by cmStudent on 2021/11/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
