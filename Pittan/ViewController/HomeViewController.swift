d45845d558e5a634ea45967b0479880c6203f38c
Taguchi-wq <20cm0123@jec.ac.jp>
2021-11-30 02:59:17 +0900

    MARKコメント追加

--- Pittan/ViewController/HomeViewController.swift
+++ Pittan/ViewController/HomeViewController.swift
@@ -8,11 +8,18 @@
 import UIKit
 
 class HomeViewController: UIViewController {
-
+    
+    // MARK: - @IBOutlets
     /// 設置場所追加ボタン
     @IBOutlet private weak var addPlaceButton: UIButton!
+    
+    
+    // MARK: - Override Methods
     override func viewDidLoad() {
         super.viewDidLoad()
+    
+    
+    // MARK: - @IBActions
     /// addPlaceButtonを押した時に呼ばれる
     @IBAction private func tappedAddPlaceButton(_ sender: UIButton) {
     }
