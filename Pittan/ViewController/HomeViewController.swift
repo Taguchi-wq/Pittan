d8299cb4096cbfd1d5fe8f21ac72559746a9f384
Taguchi-wq <20cm0123@jec.ac.jp>
2021-11-30 02:58:40 +0900

    設置場所追加ボタンを設置

--- Pittan/ViewController/HomeViewController.swift
+++ Pittan/ViewController/HomeViewController.swift
@@ -9,8 +9,12 @@ import UIKit
 
 class HomeViewController: UIViewController {
 
+    /// 設置場所追加ボタン
+    @IBOutlet private weak var addPlaceButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
+    /// addPlaceButtonを押した時に呼ばれる
+    @IBAction private func tappedAddPlaceButton(_ sender: UIButton) {
     }
     
 }
