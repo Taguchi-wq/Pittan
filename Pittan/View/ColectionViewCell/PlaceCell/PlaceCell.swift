22b6e924f6f6e257f7068efc64b0022ce7134b8e
Taguchi-wq <20cm0123@jec.ac.jp>
2021-11-30 20:08:31 +0900

    PlaceCellのUIの枠組みを作成

---
+++ Pittan/View/ColectionViewCell/PlaceCell/PlaceCell.swift
@@ -0,0 +1,17 @@
+//
+//  PlaceCell.swift
+//  Pittan
+//
+//  Created by cmStudent on 2021/11/30.
+//
+
+import UIKit
+
+class PlaceCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
