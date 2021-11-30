c439b1f8037abfdfea01793b2bfe729af7834938
Taguchi-wq <20cm0123@jec.ac.jp>
2021-11-30 22:45:03 +0900

    PlaceCellのイニシャライザを作成

--- Pittan/View/ColectionViewCell/PlaceCell/PlaceCell.swift
+++ Pittan/View/ColectionViewCell/PlaceCell/PlaceCell.swift
@@ -12,6 +12,13 @@ class PlaceCell: UICollectionViewCell {
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
+    // MARK: - Initialize
+    func initialize(place: Place) {
+        placeImageView.image  = UIImage(named: place.imageName)
+        placeNameLabel.text   = place.name
+        placeHeightLabel.text = "\(place.height)"
+        placeWidthLabel.text  = "\(place.width)"
+        categoryLabel.text    = place.category
     }
 
 }
