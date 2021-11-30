bd1245230a16f121663a3529c1c8d0bdecae4633
Taguchi-wq <20cm0123@jec.ac.jp>
2021-11-30 22:42:41 +0900

    Placeモデルを作成

---
+++ Pittan/Model/Place.swift
@@ -0,0 +1,16 @@
+//
+//  Place.swift
+//  Pittan
+//
+//  Created by cmStudent on 2021/11/30.
+//
+
+import Foundation
+
+struct Place {
+    let name: String
+    let imageName: String
+    let category: String
+    let height: Double
+    let width: Double
+}
