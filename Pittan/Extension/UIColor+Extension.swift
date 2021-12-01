9836a90c8f764aa1ba10675659a880d5c4d3cf49
Taguchi-wq <20cm0123@jec.ac.jp>
2021-12-01 10:02:54 +0900

    UIColorにカスタムカラーを追加

---
+++ Pittan/Extension/UIColor+Extension.swift
@@ -0,0 +1,22 @@
+//
+//  UIColor+Extension.swift
+//  Pittan
+//
+//  Created by cmStudent on 2021/11/30.
+//
+
+import UIKit
+
+extension UIColor {
+    
+    /// アプリのメインカラー
+    class var appMain: UIColor {
+        return UIColor(named: "appMain")!
+    }
+    
+    /// アプリのバックグラウンドカラー
+    class var appBackground: UIColor {
+        return UIColor(named: "appBackground")!
+    }
+    
+}
