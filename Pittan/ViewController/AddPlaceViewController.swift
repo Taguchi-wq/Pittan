1995e993cab544dc43fa60a7240390718a612356
Taguchi-wq <20cm0123@jec.ac.jp>
2021-12-02 10:50:10 +0900

    設置場所追加画面を作成

---
+++ Pittan/ViewController/AddPlaceViewController.swift
@@ -0,0 +1,29 @@
+//
+//  AddPlaceViewController.swift
+//  Pittan
+//
+//  Created by cmStudent on 2021/12/02.
+//
+
+import UIKit
+
+class AddPlaceViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
