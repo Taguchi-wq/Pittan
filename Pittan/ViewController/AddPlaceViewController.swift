3dd1346f07750f5eb21a1dee1c16a01aa178cafe
Taguchi-wq <20cm0123@jec.ac.jp>
2021-12-03 18:36:20 +0900

    コメントを追加

--- Pittan/ViewController/AddPlaceViewController.swift
+++ Pittan/ViewController/AddPlaceViewController.swift
@@ -7,14 +7,28 @@
 
 import UIKit
 
-class AddPlaceViewController: UIViewController {
-
 final class AddPlaceViewController: UIViewController {
+    
+    // MARK: - Properties
+    /// 画面を閉じるボタン
+    @IBOutlet private weak var closeButton: UIBarButtonItem!
+    /// 入力したデータを保存するボタン
+    @IBOutlet private weak var saveButton: UINavigationItem!
+    
+    
+    // MARK: - Override Methods
     override func viewDidLoad() {
         super.viewDidLoad()
     }
     
+    
+    // MARK: - @IBActions
+    /// closeButtonを押した時に呼ばれる
     @IBAction private func tappedCloseButton(_ sender: UIBarButtonItem) {
         dismiss(animated: true)
     }
+    
+    /// saveButtonを押した時に呼ばれる
+    @IBAction private func tappedSaveButton(_ sender: UIBarButtonItem) {}
+    
 }
