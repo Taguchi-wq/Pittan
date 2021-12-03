91530670e14d75b46dcd5bd7c351268190d97908
Taguchi-wq <20cm0123@jec.ac.jp>
2021-12-03 18:29:03 +0900

    addボタンを押した時、設置場所追加画面に遷移

--- Pittan/Extension/UIStoryboard+Extension.swift
+++ Pittan/Extension/UIStoryboard+Extension.swift
@@ -15,4 +15,10 @@ extension UIStoryboard {
         instantiateViewController(withIdentifier: type.className) as! T
     }
     
+    /// UINavigationControllerを挟んだ画面遷移をする
+    /// - Returns: <#description#>
+    func instantiateNavigationController<T: UIViewController>(with type: T.Type) -> UINavigationController {
+        instantiateViewController(withIdentifier: type.className) as! UINavigationController
+    }
+    
 }
--- Pittan/ViewController/HomeViewController.swift
+++ Pittan/ViewController/HomeViewController.swift
@@ -63,8 +63,9 @@ class HomeViewController: UIViewController {
     // MARK: - @IBActions
     /// addPlaceButtonを押した時に呼ばれる
     @IBAction private func tappedAddPlaceButton(_ sender: UIButton) {
-        RealmManager.shared.savePlace()
-        placeCollectionView.reloadData()
+        guard let addPlaceVC = storyboard?.instantiateNavigationController(with: AddPlaceViewController.self) else { return }
+        addPlaceVC.modalPresentationStyle = .fullScreen
+        present(addPlaceVC, animated: true)
     }
     
 }
