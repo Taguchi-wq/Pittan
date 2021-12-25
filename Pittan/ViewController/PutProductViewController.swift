6fee035bac5f58f153ab1178cd4fab1f08a579e5
Taguchi-wq <20cm0123@jec.ac.jp>
2021-12-26 04:34:43 +0900

    ARViewを配置

--- Pittan/ViewController/PutProductViewController.swift
+++ Pittan/ViewController/PutProductViewController.swift
@@ -6,9 +6,16 @@
 //
 
 import UIKit
+import RealityKit
 
 final class PutProductViewController: UIViewController {
 
+    // MARK: - @IBOutlets
+    /// ARView
+    @IBOutlet private weak var arView: ARView!
+    
+    
+    // MARK: - Override Methods
     override func viewDidLoad() {
         super.viewDidLoad()
     }
