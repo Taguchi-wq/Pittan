4f00808ed236af8ff6f3bc4bcb7be7aa244943f0
Taguchi-wq <20cm0123@jec.ac.jp>
2022-01-03 05:26:33 +0900

    製品のモーダルを作成

---
+++ Pittan/View/ColectionViewCell/ProductCell/ProductCell.swift
@@ -0,0 +1,34 @@
+//
+//  ProductCell.swift
+//  Pittan
+//
+//  Created by cmStudent on 2022/01/03.
+//
+
+import UIKit
+
+final class ProductCell: UICollectionViewCell {
+    
+    // MARK: - @IBOutlets
+    /// 製品の画像を表示するUIImageView
+    @IBOutlet private weak var imageView: UIImageView!
+    /// 製品の名前を表示するUILabel
+    @IBOutlet private weak var nameLabel: UILabel!
+
+    
+    // MARK: - Override Methods
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        setupLayout()
+    }
+
+    
+    // MARK: - Methods
+    /// ProductCellのレイアウトを設定する
+    private func setupLayout() {
+        imageView.cornerRadius = 10
+        imageView.addBorder(color: .black, cornerRadius: 10)
+    }
+    
+}
--- Pittan/ViewController/PutProductViewController.swift
+++ Pittan/ViewController/PutProductViewController.swift
@@ -9,15 +9,34 @@ import UIKit
 import RealityKit
 
 final class PutProductViewController: UIViewController {
+    
+    // MARK: - Properties
+    /// 製品を格納する配列
+    private var products: [String] = ["", "", "", "", "", "", "", "", "", ""]
+    
 
     // MARK: - @IBOutlets
     /// ARView
     @IBOutlet private weak var arView: ARView!
+    /// 製品を選ぶUICollectionView
+    @IBOutlet private weak var putProductCollectionView: UICollectionView!
     
     
     // MARK: - Override Methods
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        setupLayout()
+    }
+    
+    
+    // MARK: - Methods
+    /// レイアウトを設定する
+    private func setupLayout() {
+        putProductCollectionView.dataSource = self
+        putProductCollectionView.delegate = self
+        putProductCollectionView.register(cellType: ProductCell.self, bundle: nil)
+        putProductCollectionView.collectionViewLayout = createLayout()
     }
 
     
@@ -35,6 +54,67 @@ final class PutProductViewController: UIViewController {
     /// putボタンを押した時の処理
     @IBAction private func tappedPutButton(_ sender: UIButton) {
         
+    }
+    
+}
+
+
+// MARK: - UICollectionViewDataSource
+extension PutProductViewController: UICollectionViewDataSource {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return products.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let productCell = collectionView.reusableCell(with: ProductCell.self, for: indexPath)
+        return productCell
+    }
+    
+}
+
+
+// MARK: - UICollectionViewDelegate
+extension PutProductViewController: UICollectionViewDelegate {}
+
+
+// MARK: - CollectionViewLayout
+extension PutProductViewController: CollectionViewLayout {
+    
+    /// collectionViewのレイアウトを作成する
+    /// - Returns: collectionViewのレイアウト
+    func createLayout() -> UICollectionViewLayout {
+        let layout = UICollectionViewCompositionalLayout {
+            (sectionIndex: Int, layoutEnvironment: NSCollectionLayoutEnvironment) -> NSCollectionLayoutSection? in
+            return self.createProductSectionLayout()
+        }
+        
+        return layout
+    }
+    
+    /// 製品を表示するcollectionViewのセクションレイアウトを作成する
+    /// - Returns: collectionViewのセクションレイアウト
+    private func createProductSectionLayout() -> NSCollectionLayoutSection {
+        let itemSize = NSCollectionLayoutSize(
+            widthDimension: .fractionalWidth(1.0),
+            heightDimension: .fractionalHeight(1.0)
+        )
+        
+        let item = NSCollectionLayoutItem(layoutSize: itemSize)
+        
+        let groupSize = NSCollectionLayoutSize(
+            widthDimension: .fractionalWidth(0.2),
+            heightDimension: .fractionalWidth(0.3)
+        )
+        
+        let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitems: [item])
+        
+        let section = NSCollectionLayoutSection(group: group)
+        section.orthogonalScrollingBehavior = .continuous
+        section.interGroupSpacing = 16
+        section.contentInsets = .init(top: 20, leading: 16, bottom: 0, trailing: 16)
+        
+        return section
     }
     
 }
