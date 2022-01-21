5fc5e40e4fd6abef4bd2b334619fb8c256f264c2
Taguchi-wq <20cm0123@jec.ac.jp>
2022-01-21 12:09:03 +0900

    PutProductViewControllerのextensionをファイルわけする

---
+++ Pittan/ViewController/PutProductViewController/PutProductViewController+ARCoachingOverlayViewDelegate.swift
@@ -0,0 +1,33 @@
+//
+//  PutProductViewController+ARCoachingOverlayViewDelegate.swift
+//  Pittan
+//
+//  Created by cmStudent on 2022/01/21.
+//
+
+import UIKit
+import ARKit
+
+// MARK: - ARCoachingOverlayViewDelegate
+extension PutProductViewController: ARCoachingOverlayViewDelegate {
+    
+    /// コーチングを設定する
+    /// - Parameter goal: コーチングのゴール
+    func setupCoachingOverlay(_ goal:  ARCoachingOverlayView.Goal) {
+        let coachingOverlay = ARCoachingOverlayView()
+        coachingOverlay.session = sceneView.session
+        coachingOverlay.delegate = self
+        coachingOverlay.activatesAutomatically = true
+        coachingOverlay.translatesAutoresizingMaskIntoConstraints = false
+        coachingOverlay.goal = goal
+        view.addSubview(coachingOverlay)
+        
+        NSLayoutConstraint.activate([
+            coachingOverlay.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            coachingOverlay.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            coachingOverlay.widthAnchor.constraint(equalTo: view.widthAnchor),
+            coachingOverlay.heightAnchor.constraint(equalTo: view.heightAnchor)
+        ])
+    }
+    
+}
---
+++ Pittan/ViewController/PutProductViewController/PutProductViewController+UICollectionView.swift
@@ -0,0 +1,82 @@
+//
+//  PutProductViewController+UICollectionView.swift
+//  Pittan
+//
+//  Created by cmStudent on 2022/01/21.
+//
+
+import UIKit
+import SceneKit
+
+// MARK: - UICollectionViewDataSource
+extension PutProductViewController: UICollectionViewDataSource {
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return Section.allCases.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        let section = Section.allCases[section]
+        switch section {
+        case .tag: return Tag.allCases.count
+        case .product: return selectTag == .put ? products.count : 1
+        }
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let section = Section.allCases[indexPath.section]
+        switch section {
+        case .tag:
+            let tagCell = collectionView.reusableCell(with: TagCell.self, for: indexPath)
+            let tag = Tag.allCases[indexPath.item]
+            if tag == selectTag {
+                tagCell.initialize(title: tag.japanese, textColor: .appMain)
+            } else {
+                tagCell.initialize(title: tag.japanese)
+            }
+            return tagCell
+        case .product:
+            if selectTag == .put {
+                let productCell = collectionView.reusableCell(with: ProductCell.self, for: indexPath)
+                return productCell
+            } else {
+                let sizeSliderCell = collectionView.reusableCell(with: SizeSliderCell.self, for: indexPath)
+                sizeSliderCell.delegate = self
+                return sizeSliderCell
+            }
+        }
+    }
+    
+}
+
+
+// MARK: - UICollectionViewDelegate
+extension PutProductViewController: UICollectionViewDelegate {
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        let section = Section.allCases[indexPath.section]
+        switch section {
+        case .tag:
+            selectTag = .allCases[indexPath.item]
+            putProductCollectionView.reloadData()
+        case .product:
+            guard objectInteraction.selectedObject == nil else { return }
+            guard let query = sceneView.getRaycastQuery(from: sceneView.screenCenter),
+                  let result = sceneView.castRay(for: query).first,
+                  let scene = SCNScene(named: "curtain.scn"),
+                  let node = (scene.rootNode.childNode(withName: "material_1", recursively: false)) else { return }
+            objectInteraction.selectedObject = node
+            objectInteraction.selectedObject?.simdWorldPosition = result.worldTransform.translation
+            objectInteraction.selectedObject?.pivot = SCNMatrix4MakeTranslation(0, objectInteraction.selectedObject!.boundingBox.min.y, 0)
+            objectInteraction.selectedObject?.scale = SCNVector3(0.05, 0.05, 0.05)
+            
+            let materials = objectInteraction.selectedObject!.geometry!.materials
+            for material in materials where material.name == "Default_OBJ" {
+                material.diffuse.contents = UIImage(named: "00001")
+            }
+            
+            sceneView.scene.rootNode.addChildNode(objectInteraction.selectedObject!)
+        }
+    }
+    
+}
