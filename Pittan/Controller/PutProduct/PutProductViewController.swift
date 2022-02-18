f1990b55c52e53d521c813cdfb5716d477961178
Taguchi-wq <20cm0123@jec.ac.jp>
2022-02-18 17:46:08 +0900

    JonContextMenuDelegateをファイル分け

--- Pittan/Controller/PutProduct/PutProductViewController.swift
+++ Pittan/Controller/PutProduct/PutProductViewController.swift
@@ -198,29 +198,3 @@ final class PutProductViewController: UIViewController, ARSessionDelegate {
     }
     
 }
-
-// MARK: - JonContextMenuDelegate
-extension PutProductViewController: JonContextMenuDelegate {
-    
-    func menuOpened() {
-        UIImpactFeedbackGenerator(style: .light).impactOccurred()
-    }
-    
-    func menuItemWasSelected(item: JonItem) {
-        guard let index = item.id else { return }
-        let pattern = PatternKind.allCases[index].imageName
-        if let selectedObject = objectInteraction.selectedObject {
-            selectedObject.setTexture(pattern)
-            objectInteraction.selectedPattern = pattern
-        }
-    }
-    
-    func menuItemWasActivated(item: JonItem) {
-        UIImpactFeedbackGenerator(style: .light).impactOccurred()
-    }
-    
-    func menuClosed() {}
-    
-    func menuItemWasDeactivated(item: JonItem) {}
-    
-}
