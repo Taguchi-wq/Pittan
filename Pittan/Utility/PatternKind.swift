3126cca4cd5230bca7cfd17c32bb8dd7c0ad3818
Taguchi-wq <20cm0123@jec.ac.jp>
2022-02-19 15:28:43 +0900

    柄を増やす

--- Pittan/Utility/PatternKind.swift
+++ Pittan/Utility/PatternKind.swift
@@ -8,7 +8,7 @@
 import Foundation
 
 public enum PatternKind: CaseIterable {
-    case beige, blue, brown, gray, navy, rose, turquoiseBlue, yellowgreen
+    case beige, blue, brown, gray, rose, turquoiseBlue, yellowgreen, darkGray
     
     var imageName: String {
         switch self {
@@ -16,10 +16,11 @@ public enum PatternKind: CaseIterable {
         case .blue: return "blue"
         case .brown: return "brown"
         case .gray: return "gray"
-        case .navy: return "navy"
+//        case .navy: return "navy"
         case .rose: return "rose"
         case .turquoiseBlue: return "turquoise_blue"
         case .yellowgreen: return "yellowgreen"
+        case .darkGray: return "dark_gray"
         }
     }
     
@@ -29,10 +30,11 @@ public enum PatternKind: CaseIterable {
         case .blue: return "ブルー"
         case .brown: return "ブラウン"
         case .gray: return "グレー"
-        case .navy: return "ネイビー"
+//        case .navy: return "ネイビー"
         case .rose: return "ローズ"
         case .turquoiseBlue: return "ターコイズブルー"
         case .yellowgreen: return "イエローグリーン"
+        case .darkGray: return "ダークグレー"
         }
     }
 }
